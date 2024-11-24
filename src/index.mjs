d55a63385c3957b2e0b110fc7dae094ff900fa3b
Orn Bona <ornbona861@gmail.com>
2024-11-24 16:26:41 +0700

    get request

--- src/index.mjs
+++ src/index.mjs
@@ -4,7 +4,29 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const users = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Den Smith' },
+    { id: 3, name: 'Jane Doe' },
+];
 
+const products = [
+    { id: 1, name: 'Product 1', price: 100 },
+    { id: 2, name: 'Product 2', price: 200 },
+    { id: 3, name: 'Product 3', price: 300 },
+];
+
+app.get('/', (req, res) => {
+    res.status(201).send({ message: 'Hello World!' });
+});
+
+app.get('/api/users', (req, res) => {
+    res.status(200).send(users);
+});
+
+app.get('/api/products', (req, res) => {
+    res.status(200).send(products);
+});
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
